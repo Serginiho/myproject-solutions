2ff50c3db08c304697f6964aa3391179b4d6ca37
Serginiho <serginiho2985@gmail.com>
2021-08-10 20:01:23 +0300

    Refactoring name of class

--- src/com/homecompany/chapter21/exercise2/FibonacciTaskInThreads.java
+++ src/com/homecompany/chapter21/exercise2/FibonacciTaskInThreads.java
@@ -1,6 +1,6 @@
 package com.homecompany.chapter21.exercise2;
 
-public class FibonacciTaskInThread {
+public class FibonacciTaskInThreads {
     public static void main(String[] args) {
             new Thread(new FibonacciTask1(10)).start();
             new Thread(new FibonacciTask2(15)).start();
